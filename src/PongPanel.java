b91df6b8a329c9644dfd7f88f2046f7c65e43f06
sam-forbes <92132139+sam-forbes@users.noreply.github.com>
2022-04-25 12:50:00 +1200

    Added in Graphics painting

--- src/PongPanel.java
+++ src/PongPanel.java
@@ -3,13 +3,22 @@ import java.awt.Color;
    import java.awt.event.ActionListener;
    import java.awt.event.KeyEvent;
    import java.awt.event.KeyListener;
+   import javax.swing.Timer;
    import javax.swing.JPanel;
+   import java.awt.Graphics;
+   import java.awt.Graphics2D;
    
    public class PongPanel extends JPanel implements ActionListener, KeyListener {
-       
-      public PongPanel() {
-          setBackground(Color.BLACK);
-      }
+	   private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	   private final static int TIMER_DELAY = 5;
+	        
+	    public PongPanel() {
+	        setBackground(BACKGROUND_COLOUR);
+	        Timer timer = new Timer(TIMER_DELAY, this);
+	           timer.start();
+	   }
+
+   
   
       @Override
       public void keyPressed(KeyEvent event) {
@@ -25,6 +34,18 @@ import java.awt.Color;
   
       @Override
       public void actionPerformed(ActionEvent event) {
+    	  update();
+    	  repaint();
       }
   
+      
+      private void update() {
+          
+      }
+      @Override
+      public void paintComponent(Graphics g) {
+          super.paintComponent(g);
+          g.setColor(Color.WHITE);
+          g.fillRect(20, 20, 100, 100);
+      }
  }
