c3836a3f5f0383fcac017cdc8435df58fc9a39b0
sam-forbes <92132139+sam-forbes@users.noreply.github.com>
2022-04-25 12:37:59 +1200

    Game now has board

--- src/Pong.java
+++ src/Pong.java
@@ -1,9 +1,25 @@
-
-public class Pong {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			System.out.println("Hello Development Branch ");
-	}
-
-}
+ import javax.swing.JFrame;
+   
+   public class Pong extends JFrame {
+       
+       private final static String WINDOW_TITLE = "Pong";
+       private final static int WINDOW_WIDTH = 800;
+       private final static int WINDOW_HEIGHT = 600;
+       
+       public Pong() {
+              setTitle(WINDOW_TITLE);
+              setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+              setResizable(false);
+              add(new PongPanel());
+              setVisible(true);
+              setDefaultCloseOperation(EXIT_ON_CLOSE);
+      }
+  
+       public static void main(String[] args) {      
+           javax.swing.SwingUtilities.invokeLater(new Runnable() {
+             public void run() {
+                    new Pong();
+             }
+           });
+       }
+   }
---
+++ src/PongPanel.java
@@ -0,0 +1,30 @@
+import java.awt.Color;
+   import java.awt.event.ActionEvent;
+   import java.awt.event.ActionListener;
+   import java.awt.event.KeyEvent;
+   import java.awt.event.KeyListener;
+   import javax.swing.JPanel;
+   
+   public class PongPanel extends JPanel implements ActionListener, KeyListener {
+       
+      public PongPanel() {
+          setBackground(Color.BLACK);
+      }
+  
+      @Override
+      public void keyPressed(KeyEvent event) {
+      }
+  
+      @Override
+      public void keyReleased(KeyEvent event) {
+      }
+  
+      @Override
+      public void keyTyped(KeyEvent event) {
+      }
+  
+      @Override
+      public void actionPerformed(ActionEvent event) {
+      }
+  
+ }
